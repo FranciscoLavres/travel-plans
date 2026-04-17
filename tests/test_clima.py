2f0ce4adf7cc139f54ca38b4bcf3ae59f99fe423
Francisco Lavres <franciscolavres45@gmail.com>
2026-04-17 15:50:26 -0300

    100%

--- tests/test_clima.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from src.api import obter_clima
-
-
-class TestClima(unittest.TestCase):
-
-    def test_clima_valido(self):
-        self.assertIn(obter_clima("Aracaju"), ["ruim", "bom", "normal"])
